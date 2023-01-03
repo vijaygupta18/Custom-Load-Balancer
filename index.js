0c3965d34833699b68539ad4710243b0cc3b84e3
vijaygupta <vijay.gupta@vahan.co>
2023-01-03 13:45:57 +0530

    added new function

--- index.js
+++ index.js
@@ -1,30 +1,49 @@
 const express = require('express')
 const app = express()
 const axios = require('axios');
-const serverList=require('./serverList')
+const serverList = require('./serverList')
 
 let currentServerIndex = 0, currentServer;
-
-const handler = async (req, res) => {
-  currentServer = serverList[currentServerIndex];
-  currentServerIndex === (serverList.length - 1) ? currentServerIndex = 0 : currentServerIndex++;
-  try {
-    const response = await axios.get(currentServer);
-    console.log(`successful proxy to the ${currentServer}`);
-    res.send(response.data);
-  }
-  catch (err) {
-    console.log(`failed proxy to  the -> ${currentServer}`);
-    handler(req, res);
-  }
-}
-
-app.use((req, res) => {
-  handler(req, res);
-})
 
 app.listen(80, function (err) {
   if (err) console.log("Error in server setup");
   console.log("Server listening on Port", 80);
+  setInterval(() => {
+    healthCheckCurrentServer();
+  }, 10000);
+})
+
+const handler = async (req, res) => {
+  currentServer = serverList[currentServerIndex];
+  currentServerIndex++;
+  currentServerIndex = currentServerIndex % (serverList.length);
+  try {
+    const response = await axios.get(currentServer);
+    console.log('running',currentServer);
+    res.send(response.data);
+  }
+  catch (err) {
+    console.log(`server ${currentServer} failed running ${serverList[currentServerIndex]}`);
+    handler(req, res);
+  }
+}
+
+const healthCheckCurrentServer = async () => {
+  if (currentServer) {
+    try {
+      console.log('Running health check for', currentServer);
+      const response = await axios.get(currentServer);
+      if (response) {
+        console.log(`server ${currentServer} is healthy`);
+      }
+    }
+    catch (e) {
+      console.log(`health of server ${currentServer} failed running ${serverList[currentServerIndex]}`);
+    }
+
+  }
+}
+app.use((req, res) => {
+  handler(req, res);
 })
 
--- serverList.js
+++ serverList.js
@@ -1,5 +1,6 @@
 const serverList = [
     "http://localhost:8000",
-    "http://localhost:8001"
+    "http://localhost:8001",
+    "http://localhost:8002"
   ]
   module.exports = serverList;
